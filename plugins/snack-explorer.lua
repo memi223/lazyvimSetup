3acd628b92d830cd8474491e7a7e14b6c476460d
memi223 <giahuy050808@gmail.com>
2026-08-26 14:37:58 +0700

    Add snack-explorer plugin configuration

---
+++ plugins/snack-explorer.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "folke/snacks.nvim",
+        opts = {
+            picker = {
+                hidden = true, -- show dotfiles/dotfolders
+                ignored = true, -- show files/folders matched by .gitignore
+            },
+        },
+    },
+}
